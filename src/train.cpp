847cb73fa44535347ffd0035948b50c9fa66341e
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-05-11 22:58:22 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,36 +1,35 @@
-// Copyright 2021 NNTU-CS
-#include "train.h"
-Train::Train() {
-    first = nullptr;
-    countOp = 0;
-}
-Cage {false, nullptr, nullptr};
-
-void Train::addCage(bool light2) {
-  Cage *newCage = new Cage;
-  newCage->light = light2;
-
-  if (first == nullptr) {
-      first = newCage;
-      newCage->next = first;
-      newCage->prev = first;
-  } else {
-      Cage *lastCage = first->prev;
-      newCage->next = first;
-      newCage->prev = lastCage;
-      lastCage->next = newCage;
-      first->prev = newCage;
-  }
-}
-int Train::getLength() {
-  int length = 0;
-  Cage *currentCage = first;
-  do {
-      length++;
-      currentCage = currentCage->next;
-  } while (currentCage != first);
-  return length;
-}
-int Train::getOpCount() {
-  return countop;
+#include "train.h" 
+Train::Train() { 
+    first = nullptr; 
+    countOp = 0; 
+} 
+Cage {false, nullptr, nullptr}; 
+ 
+void Train::addCage(bool light2) { 
+  Cage *newCage = new Cage; 
+  newCage->light = light2; 
+ 
+  if (first == nullptr) { 
+      first = newCage; 
+      newCage->next = first; 
+      newCage->prev = first; 
+  } else { 
+      Cage *lastCage = first->prev; 
+      newCage->next = first; 
+      newCage->prev = lastCage; 
+      lastCage->next = newCage; 
+      first->prev = newCage; 
+  } 
+} 
+int Train::getlength() { 
+  int length = 0; 
+  Cage *currentCage = first; 
+  do { 
+      length++; 
+      currentCage = currentCage->next; 
+  } while (currentCage != first); 
+  return length; 
+} 
+int Train::getopcount() { 
+  return countOp; 
 }
