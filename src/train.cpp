7883ae32cdbc692428e99979a9d933c03d8cbc49
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-05-11 22:16:59 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -12,36 +12,36 @@ cage *first;
 
  public:
 Train() {
- countop = 0;
- first = nullptr;
+countop = 0;
+first = nullptr;
 }
 
 void addcage(bool light) {
-   cage *newCage = new cage;
-   newCage->light = light;
+  cage *newCage = new cage;
+  newCage->light = light;
 
-   if (first == nullptr) {
+  if (first == nullptr) {
       first = newCage;
       newCage->next = first;
       newCage->prev = first;
-   } else {
+  } else {
       cage *lastCage = first->prev;
       newCage->next = first;
       newCage->prev = lastCage;
       lastCage->next = newCage;
       first->prev = newCage;
-   }
+  }
 }
 int getlength() {
-   int length = 0;
-   cage *currentCage = first;
-   do {
+  int length = 0;
+  cage *currentCage = first;
+  do {
       length++;
       currentCage = currentCage->next;
-   } while (currentCage != first);
-   return length;
+  } while (currentCage != first);
+  return length;
 }
 int getopcount() {
-   return countop;
+  return countop;
 }
 };
